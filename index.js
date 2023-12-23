64158caa23831696cd36faf6e1400b2b11f208e6
Mat Gargano <mat@statenweb.com>
2023-12-23 15:03:03 -0500

    intermittent update

--- index.js
+++ index.js
@@ -2,10 +2,99 @@ const fs = require("fs");
 const path = require("path");
 const copy = require("recursive-copy");
 const slugify = require("slugify");
-const recursiveRead = require('recursive-readdir'); // Ensure this package is installed
-const readline = require("readline");
-const { execSync } = require("child_process");
-const { spawnSync } = require("child_process");
+const { execSync, spawnSync } = require("child_process");
+const {rimraf} = require("rimraf");
+const recursiveRead = require('recursive-readdir');
+const archiver = require("archiver");
+
+
+
+const newFunction = `
+  function ({ addVariant }) {
+    addVariant('mobile-only', "@media screen and (max-width: theme('screens.md'))")
+  }
+`;
+
+function zipBuildResult(themeName, buildResultDirectory, downloadsDirectory) {
+  // Define the filename based on SLUGIFIED_THEME_NAME
+  const filename = `${slugify(themeName)}-build-result.zip`;
+
+  // Create a write stream for the zip file
+  const output = fs.createWriteStream(path.join(downloadsDirectory, filename));
+
+  // Create an archiver instance
+  const archive = archiver("zip", {
+    zlib: { level: 9 }, // Set compression level
+  });
+
+  // Pipe the archive data to the output file
+  archive.pipe(output);
+
+  // Add the build_result directory to the archive (recursively)
+  archive.directory(buildResultDirectory, false);
+
+  // Listen for all archive data to be written
+  output.on("close", () => {
+    console.log(`Successfully zipped and saved as: ${filename}`);
+
+    // Remove the build_result directory (recursively)
+    rimraf.sync(buildResultDirectory);
+  });
+
+  // Finalize the archive
+  archive.finalize();
+}
+function deleteGitDirectories(directoryPath) {
+  if (fs.existsSync(directoryPath)) {
+    const files = fs.readdirSync(directoryPath);
+    files.forEach((file) => {
+      const filePath = path.join(directoryPath, file);
+      if (fs.statSync(filePath).isDirectory()) {
+        if (file === ".git") {
+          // Delete the .git directory
+          deleteFolderRecursive(filePath);
+        } else {
+          // Recursively check subdirectories
+          deleteGitDirectories(filePath);
+        }
+      }
+    });
+  }
+}
+
+function deleteFolderRecursive(directoryPath) {
+  if (fs.existsSync(directoryPath)) {
+    fs.readdirSync(directoryPath).forEach((file) => {
+      const filePath = path.join(directoryPath, file);
+      if (fs.statSync(filePath).isDirectory()) {
+        deleteFolderRecursive(filePath);
+      } else {
+        fs.unlinkSync(filePath);
+      }
+    });
+    fs.rmdirSync(directoryPath);
+  }
+}
+const findAndReplaceInFile = (filePath, replacements) => {
+  let content = fs.readFileSync(filePath, "utf8");
+  for (const key in replacements) {
+    const search = `{${key}}`;
+    const replace = replacements[key];
+    content = content.replace(new RegExp(search, "g"), replace);
+  }
+  fs.writeFileSync(filePath, content);
+};
+
+async function applyFindAndReplaceToAllFiles(directory, replacements) {
+  try {
+    const files = await recursiveRead(directory);
+    files.forEach(file => {
+      findAndReplaceInFile(file, replacements);
+    });
+  } catch (error) {
+    console.error("Error processing files:", error);
+  }
+}
 // Function to read JSON configuration file
 function readConfigFile(filePath) {
   try {
@@ -17,26 +106,24 @@ function readConfigFile(filePath) {
   }
 }
 
-// Function to find and replace in files
-const findAndReplaceInFile = (filePath, replacements) => {
-  let content = fs.readFileSync(filePath, "utf8");
-  for (const [search, replace] of replacements) {
-    content = content.replace(new RegExp(`${search}`, "g"), replace);
-  }
-  fs.writeFileSync(filePath, content);
-};
-
-// Function to apply find-and-replace to all files in a directory
-async function applyFindAndReplaceToAllFiles(directory, replacements) {
-  try {
-    const files = await recursiveRead(directory);
-    files.forEach(file => {
-      findAndReplaceInFile(file, replacements);
+// Function to copy files from source to target directory
+const copyFiles = (sourceDir, targetDir) => {
+  return new Promise((resolve, reject) => {
+    copy(sourceDir, targetDir, {
+      overwrite: true,
+      expand: true,
+      dot: true
+    }, (error) => {
+      if (error) {
+        console.error("Error:", error);
+        reject(error);
+      } else {
+        console.log(`Files copied from ${sourceDir} to ${targetDir}.`);
+        resolve();
+      }
     });
-  } catch (error) {
-    console.error("Error processing files:", error);
-  }
-}
+  });
+};
 
 // Main execution
 (async () => {
@@ -47,47 +134,61 @@ async function applyFindAndReplaceToAllFiles(directory, replacements) {
     process.exit(1);
   }
   const config = readConfigFile(configFilePath);
-  const additionalReplacements = Object.entries(config).map(([key, value]) => [`{${key}}`, value]);
-
-  const scriptRootDirectory = process.cwd();
-  const buildResultDirectory = `${scriptRootDirectory}/build_result`;
-  const customizationDirectory = `${scriptRootDirectory}/custom_templates`;
-  const rootCustomizationDirectory = `${customizationDirectory}/[root]`;
-  const themeCustomizationDirectory = `${customizationDirectory}/[theme]`;
-  const composerFileLocation = `${rootCustomizationDirectory}/composer.json`;
-
-  const fileHeaderCssFile = `${buildResultDirectoryThemeBase}/tailwind/file-header.css`;
-
-
-
-  const slugifiedThemeName = slugify(config["THEME_NAME"], {
+  config['SLUGIFIED_THEME_NAME'] = slugify(config["THEME_NAME"], {
     lower: true,
     remove: /[^a-zA-Z0-9 -]/g,
     replacement: "-",
   });
-  const buildResultDirectoryThemeBase = `${buildResultDirectory}/web/wp-content/themes/${slugifiedThemeName}`;
+  const scriptRootDirectory = process.cwd();
+  const buildResultDirectory = `${scriptRootDirectory}/build_result`;
+  const customizationDirectory = `${scriptRootDirectory}/custom_templates`;
 
-  if (fs.existsSync(buildResultDirectory)) {
-    fs.rmSync(buildResultDirectory, { recursive: true, force: true });
+
+  const downloadsDirectory = path.join(__dirname, "downloads")
+  if (!fs.existsSync(downloadsDirectory)) {
+    fs.mkdirSync(downloadsDirectory);
   }
 
-  fs.mkdirSync(`${buildResultDirectoryThemeBase}`, { recursive: true });
+  if (fs.existsSync(buildResultDirectory)) {
+    rimraf.sync(buildResultDirectory); // Use rimraf to remove the directory and its contents
+  }
 
-  const { spawnSync } = require("child_process");
+
+
+  // Delete the build_result directory recursively if it exists
+  if (fs.existsSync(buildResultDirectory)) {
+    fs.rmdirSync(buildResultDirectory, { recursive: true });
+  }
+
+  // Ensure build_result directory exists
+  fs.mkdirSync(buildResultDirectory, { recursive: true });
+
+  // Copy files from [root] to build_result
+  await copyFiles(`${customizationDirectory}/[root]`, buildResultDirectory);
+
+  // Create the theme directory
+  const themeDirectory = `${buildResultDirectory}/web/wp-content/themes/${config["SLUGIFIED_THEME_NAME"]}`;
+  fs.mkdirSync(themeDirectory, { recursive: true });
+
+  // Clone the _tw repository into the theme directory
   spawnSync(
       "git",
-      ["clone", "git@github.com:gregsullivan/_tw.git", buildResultDirectoryThemeBase],
+      ["clone", "git@github.com:gregsullivan/_tw.git", themeDirectory],
       { stdio: "inherit" }
   );
 
+  await copyFiles(`${customizationDirectory}/[theme]`, themeDirectory);
+  const buildResultDirectoryThemeBase = `${buildResultDirectory}/web/wp-content/themes/${config["SLUGIFIED_THEME_NAME"]}`;
+
   const tailwindConfigLocation = `${buildResultDirectoryThemeBase}/tailwind/tailwind.config.js`;
   const configPath = path.resolve(tailwindConfigLocation);
+
   const configContent = fs.readFileSync(configPath, "utf8");
   const newFunction = `
-    function ({ addVariant }) {
-      addVariant('mobile-only', "@media screen and (max-width: theme('screens.md'))")
-    }
-  `;
+  function ({ addVariant }) {
+    addVariant('mobile-only', "@media screen and (max-width: theme('screens.md'))")
+  }
+`;
 
   const updatedConfigContent = configContent.replace(
       /(plugins:\s*\[)([\s\S]*?)(\s*]\s*)/,
@@ -99,73 +200,18 @@ async function applyFindAndReplaceToAllFiles(directory, replacements) {
 
   fs.writeFileSync(configPath, updatedConfigContent, "utf8");
 
-  fs.rmSync(`${buildResultDirectoryThemeBase}/.git`, { recursive: true });
 
-  const copyFiles = (sourceDir, targetDir) => {
-    return new Promise((resolve, reject) => {
-      copy(sourceDir, targetDir, {
-        overwrite: true,
-        expand: true,
-        dot: true,
-        filter: ['**/*', '!**/*.git'], // Add any other filters as necessary
-      }, (error) => {
-        if (error) {
-          console.error("Error:", error);
-          reject(error);
-        } else {
-          console.log(`Files copied from ${sourceDir} to ${targetDir}.`);
-          resolve();
-        }
-      });
-    });
-  };
+  // Change permissions for the .git directory
+  spawnSync(
+      "chmod",
+      ["-R", "777", `${themeDirectory}/.git`],
+      { stdio: "inherit" }
+  );
+  await applyFindAndReplaceToAllFiles(buildResultDirectory, config);
 
-  const syncDirectories = [
-    {
-      source: rootCustomizationDirectory,
-      target: buildResultDirectory,
-    },
-    {
-      source: themeCustomizationDirectory,
-      target: buildResultDirectoryThemeBase,
-    },
-  ];
-
-  for (const { source, target } of syncDirectories) {
-    await copyFiles(source, target);
-  }
-
-  // Apply find-and-replace to all files in custom_templates
-  await applyFindAndReplaceToAllFiles(customizationDirectory, additionalReplacements);
-  console.log('-------------------');
-  console.log(customizationDirectory, additionalReplacements);
-  console.log('-------------------');
-  // Modify functions.php
-  const functionsPhpPath = `${buildResultDirectoryThemeBase}/theme/functions.php`;
-  if (fs.existsSync(functionsPhpPath)) {
-    const functionsPhpContent = fs.readFileSync(functionsPhpPath, "utf8");
-    const modifiedContent = functionsPhpContent.replace(
-        "<?php",
-        `<?php\nrequire_once __DIR__ . '/application.php';`
-    );
-    fs.writeFileSync(functionsPhpPath, modifiedContent);
-    console.log("Updated functions.php.");
-  } else {
-    console.log("functions.php not found, skipping modification.");
-  }
-
-
-
-
-  // Change to build_result directory
+  // Change to build_result directory and perform composer install
   process.chdir(buildResultDirectory);
-  console.log("Changed directory to:", buildResultDirectory);
-
-  // Run composer install
-  console.log("Running composer install...");
-  execSync("composer update", { stdio: "inherit" });
-  execSync("composer install", { stdio: "inherit" });
-  console.log("Composer install completed.");
-
+  deleteGitDirectories(buildResultDirectory);
   console.log("Theme build completed!");
+  zipBuildResult(config["SLUGIFIED_THEME_NAME"], buildResultDirectory, downloadsDirectory);
 })();
