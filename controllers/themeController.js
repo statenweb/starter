b9c472b3b5afed641772186f853563c5235a8d5c
Mat Gargano <mat@statenweb.com>
2023-12-23 21:42:10 -0500

    add success in response

--- controllers/themeController.js
+++ controllers/themeController.js
@@ -7,8 +7,8 @@ exports.generateTheme = async (req, res) => {
         const themeConfig = req.body;
         const downloadLink = await themeGenerator.generateTheme(themeConfig);
         deleteOldDownloads();
-        res.status(200).json({ message: "Theme generated successfully", downloadLink, });
+        res.status(200).json({ message: "Theme generated successfully", downloadLink, success: true });
     } catch (error) {
-        res.status(500).json({ message: "Error generating theme", error: error.message });
+        res.status(500).json({ message: "Error generating theme", error: error.message, success: false });
     }
 };
