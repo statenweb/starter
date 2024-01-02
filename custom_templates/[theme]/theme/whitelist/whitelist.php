a64c85491bae5be2df41409266f22bb590f04f6a
Mat Gargano <mgargano@gmail.com>
2024-01-02 12:53:46 -0500

    update...

--- custom_templates/[theme]/theme/whitelist/whitelist.php
+++ custom_templates/[theme]/theme/whitelist/whitelist.php
@@ -1,4 +1,4 @@
-<div class="text-[#C20000] hover:text-[#00ADEF] duration-300 transition-all text-[lg] px-5 py-3"></div>
+<div class="duration-300 transition-all px-5 py-3"></div>
 
 <div class="relative mobile-only:flex mobile-only:justify-center mobile-only:-translate-y-[100%] mobile-only:overflow-y-auto"></div>
-<div class="bg-[#243B55] group-hover:bg-[#c20000] bg-navy responsive-menu menu-list"></div>
+<div class="bg-navy responsive-menu menu-list"></div>
